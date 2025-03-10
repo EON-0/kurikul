7cc4fa14878de0e9f8e48833f5b3f6a822af8d16
EON-0 <104648049+EON-0@users.noreply.github.com>
2025-03-10 19:28:20 +0100

    Add files via upload

---
+++ kukuriku/index/getAktinosti.php
@@ -0,0 +1,29 @@
+<?php
+include '../dbasse_conn.php';
+//daje sve aktivnosti za pojedninog korisnika
+
+$user_ID = $_GET["user_ID"];
+$vrstaAktivnosti_ID = $_GET["odabir"];
+
+if($vrstaAktivnosti_ID != -1){
+        $sql = "SELECT DISTINCT sk_Aktivnost.Naziv, sk_Aktivnost.ID 
+        FROM sk_Aktivnost 
+        JOIN sk_Prava ON sk_Aktivnost.ID = sk_Prava.AktivnostID
+        JOIN sk_Aktivnosti ON sk_Aktivnost.ID = sk_Aktivnosti.AktivnostID
+        WHERE sk_Prava.KorisnikID = ? AND sk_Aktivnosti.VrstaID = ? AND Aktivno = 1;";
+
+$aktivnosti_array = fetchMultipleResults($con,$sql,[$user_ID,$vrstaAktivnosti_ID]);
+}
+
+else{
+        $sql = "SELECT DISTINCT sk_Aktivnost.Naziv, sk_Aktivnost.ID 
+        FROM sk_Aktivnost 
+        JOIN sk_Prava ON sk_Aktivnost.ID = sk_Prava.AktivnostID
+        WHERE sk_Prava.KorisnikID = ? AND Aktivno = 1;";
+        $aktivnosti_array = fetchMultipleResults($con,$sql,[$user_ID]); 
+}
+
+
+
+echo json_encode($aktivnosti_array, JSON_UNESCAPED_UNICODE);
+?>
---
+++ kukuriku/login/login.php
@@ -0,0 +1,30 @@
+<?php
+session_start();
+?>
+<!DOCTYPE html>
+<html lang="hr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="login.css">
+    <script src="login.js"></script>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <title>Login</title>
+</head>
+<body>
+<div class="login-container">
+    <img src=".\logo\tsck.png" alt="Logo" class="logo">
+    
+    <div class="form-group">
+        <label for="username">Korisničko ime:</label>
+        <input type="text" id="username" name="username" required>
+    </div>
+
+    <div class="form-group">
+        <label for="password">Lozinka:</label>
+        <input type="password" id="password" name="password" required>
+    </div>
+    <button type="button" onclick="logInCheck()">LOGIN</button>
+</div>
+</body>
+</html>
