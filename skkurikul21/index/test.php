a9d6c6c378c667f46baa2151a65976a6d1cc07ef
EON-0 <104648049+EON-0@users.noreply.github.com>
2025-03-14 12:53:55 +0100

    Add files via upload

---
+++ skkurikul21/index/test.php
@@ -0,0 +1,34 @@
+<?php
+include '../dbasse_local.php';
+
+
+$user_ID = 1024;
+$aktivnost_ID = 1168;
+
+$opciPodaci = [
+    "author" => "Krešimir Kočiš",
+    "carriers" => "Razvoj softvera - grupni rad na projektu",
+    "created" => "2023-09-17",
+    "evaluation" => "",
+    "expenses" => "Razvoj softvera - grupni rad na projektu",
+    "name" => "Razvoj softvera - grupni rad na projektu",
+    "purpose" => "Zainteresiranim učenicima naše škole.",
+    "report" => "",
+    "responsibility" => "Neznam kaj je to",
+    "status" => "3",
+    "timeline" => "Kroz školsku godinu"
+];
+update($con,$opciPodaci,$aktivnost_ID);
+
+function update($con,$opciPodaci,$aktivnost_ID) {
+    $sql = "UPDATE sk_aktivnost
+    SET Naziv = ?, 
+        Namjena = ?,
+        Vremenik = ?, 
+        Datum = STR_TO_DATE(?, '%Y-%m-%d')
+    WHERE ID = ?"; 
+$parms = [$opciPodaci["name"], $opciPodaci["purpose"], $opciPodaci["timeline"], $opciPodaci["created"], $aktivnost_ID];
+placeToDataBase($con, $sql, $parms);
+
+}
+?>
